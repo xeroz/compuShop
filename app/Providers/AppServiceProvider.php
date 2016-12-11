00efdfc784f36526b02dc0bf0a99e8910d01c898
JUAN <chavezvasquezjuan@gmail.com>
2016-12-11 17:46:31 -0500

    Service Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,29 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+   /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-    }
+   public function boot()
+   {
+      //
+   }
 
-    /**
+   /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
-        //
-    }
+   public function register()
+   {
+      $path = 'App\Persistences';
+
+      $this->app->bind($path.'\Laptop\LaptopPersistenceInterface', $path.'\Laptop\LaptopPersistenceEloquent');
+      $this->app->bind($path.'\Product\ProductPersistenceInterface', $path.'\Product\ProductPersistenceEloquent');
+      $this->app->bind($path.'\WishListProduct\WishListProductPersistenceInterface', $path.'\WishListProduct\WishListProductPersistenceEloquent');
+      $this->app->bind($path.'\CategoryProduct\CategoryProductPersistenceInterface', $path.'\CategoryProduct\CategoryProductPersistenceEloquent');
+      $this->app->bind($path.'\User\UserPersistenceInterface', $path.'\User\UserPersistenceEloquent');
+   }
 }
